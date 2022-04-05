b8071de5e8711c34ed5d394f0fe66fe3ddd4438f
lies93 <hocini.lies@gmail.com>
2022-04-05 15:26:23 +0200

    step 2 done

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text,View,StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ export default function app  ()  {
+     
+  const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      backgroundColor:'blue',
+      alignItems:"center",
+      justifyContent:"center"
+      
+    },
+    text1:{
+      fontSize:30
+    },
+    text2:{
+      fontWeight:"bold",
+    },
+    text3:{
+      textAlign:"center"
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  })
+  
+  return(   
+     <>
+      <View style={styles.container}>
+          <View style={{backgroundColor:"white"}}>
+              <Text style={styles.text1}>ha</Text>
+              <Text style={styles.text2}>hi</Text>
+              <Text style={styles.text3}>hij</Text>
+          </View>
+      </View>
+     </>
+   )
+ }
